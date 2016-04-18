87efe635c20325e91791dd6b90d242adabf8f8ba
Nick van kleef <nvankleef@gmail.com>
2016-04-18 16:47:35 +0200

    FirstMusicIntegrationTest_Windows

---
+++ Benzaiten/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,74 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX_CORRECT = 212976137U;
+        static const AkUniqueID SFX_OLDMAN = 528352284U;
+        static const AkUniqueID SFX_RAIN = 1904851423U;
+        static const AkUniqueID SFX_SHAKU_D = 1123405176U;
+        static const AkUniqueID SFX_SHAKU_EB = 3790763289U;
+        static const AkUniqueID SFX_SHAKU_G = 1123405179U;
+        static const AkUniqueID STOP_D = 1452767820U;
+        static const AkUniqueID STOP_EB = 2641725269U;
+        static const AkUniqueID STOP_G = 1452767823U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSIC_SWITCH
+        {
+            static const AkUniqueID GROUP = 2724869341U;
+
+            namespace STATE
+            {
+                static const AkUniqueID INTRO = 1125500713U;
+                static const AkUniqueID MAIN = 3161908922U;
+                static const AkUniqueID RAINSONG = 405763060U;
+            } // namespace STATE
+        } // namespace MUSIC_SWITCH
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
