137289f167bfdaef325b1f40ece915c27316037c
Silver Huber <silverhuber@hotmail.com>
2016-06-01 11:51:38 +0200

    Alle scripts in het mapje

---
+++ Benzaiten/Assets/Scripts/Text_TempleBlock.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Text_TempleBlock : MonoBehaviour
+{
+	private MyText textTypeScript;
+	public Collider2D thisCollider;
+	public string textToType;
+	public string character;
+	public bool onTriggertext;
+
+
+	void Start ()
+	{
+		thisCollider = GetComponent <Collider2D> ();
+		textTypeScript = GameObject.FindGameObjectWithTag ("Text").GetComponent <MyText> ();
+		if (onTriggertext)
+			thisCollider.isTrigger = true;
+		else if (!onTriggertext)
+			thisCollider.isTrigger = false;
+	}
+
+	void Update ()
+	{
+	
+
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.name == "Player")
+		{
+			if (onTriggertext)
+				textTypeScript.TypeLine (textToType, character);
+		}
+	}
+
+	void OnCollisionEnter2D ()
+	{
+		
+		print ("fda"); 
+		if (onTriggertext == false)
+			textTypeScript.TypeLine (textToType, character);
+		
+	}
+}
